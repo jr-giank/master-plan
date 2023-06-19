9156c81743de71392ba8c5eac3f183a4439c2c35
Gianfranco De Paula <baciliogianfranco@gmail.com>
2023-06-19 14:11:04 -0400

    Database structure

---
+++ master_plan/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import work_axe, activitie, responsible, date
+
+admin.site.register(work_axe)
+admin.site.register(activitie)
+admin.site.register(responsible)
+admin.site.register(date)
--- master_plan/settings.py
+++ master_plan/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--q2pg$8q_ctf7ia&5w_3!)=#sto_xcw5l_5@@g3pk&5)ha_#bc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'master_plan',
 ]
 
 MIDDLEWARE = [
