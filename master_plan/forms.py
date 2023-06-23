9d639255523f888d61433a6f953443d2800f7531
Gianfranco De Paula <baciliogianfranco@gmail.com>
2023-06-23 08:23:52 -0400

    Create done

--- master_plan/forms.py
+++ master_plan/forms.py
@@ -1,18 +1,31 @@
 from django import forms
-from .models import Work_axe, Activitie, Responsible, Date
+from .models import WorkAxe, Activitie, Responsible, Date, MasterPlan
 
 class WorkAxeForm(forms.Form):
     name = forms.CharField(label=('Eje de trabajo'))
 
 class ActivitieForm(forms.Form):
-    class Meta:
-        model = Activitie
-        fields = '__all__'
+    work_axe = forms.ModelChoiceField(label=('Eje de trabajo'), queryset=WorkAxe.objects.all())
+    name = forms.CharField(label=('Nombre de Actividad'), widget=forms.Textarea)
 
 class ResponsibleForm(forms.Form):
-    name = forms.CharField(label=('Eje de trabajo')) 
+    name = forms.CharField(label=('Responsable')) 
 
 class DateForm(forms.Form):
     class Meta:
         model = Date
         fields = '__all__'
+
+class MasterPlanForm(forms.Form):
+    name = forms.CharField(label='Master plan')
+    description = forms.CharField(label='Descripción', widget=forms.Textarea)
+    notes = forms.CharField(label='Notas', widget=forms.Textarea)
+
+class DetailForm(forms.Form):
+    master_plan = forms.ModelChoiceField(label=('Master plan'), queryset=MasterPlan.objects.all())
+    activity = forms.ModelChoiceField(label=('Actividad'), queryset=Activitie.objects.all())
+    responsible = forms.ModelChoiceField(label=('Responsable'), queryset=Responsible.objects.all())
+    scheduled_date = forms.DateField(label=('Fecha a ejecutar'))
+    completed_date = forms.DateField(label=('Fecha ejecutada'))
+    evaluation = forms.CharField(label=('Evaluación'), widget=forms.Textarea, required=False)
+    observations = forms.CharField(label=('Observaciones'), widget=forms.Textarea, required=False)
--- master_plan/models.py
+++ master_plan/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Work_axe(models.Model):
+class WorkAxe(models.Model):
 
     """ Work axes model for the task """
 
@@ -13,7 +13,7 @@ class Activitie(models.Model):
 
     """ Activity model for the task """
 
-    work_axe = models.ForeignKey(to=Work_axe, default="", on_delete=models.SET_DEFAULT)
+    work_axe = models.ForeignKey(to=WorkAxe, default="", on_delete=models.SET_DEFAULT)
     name = models.TextField(null=False, blank=False)
     
     def __str__(self):
@@ -41,3 +41,32 @@ class Date(models.Model):
 
     def __str__(self):
         return f"{self.activitie.work_axe} - {self.activitie} - {self.date}"
+
+class MasterPlan(models.Model):
+
+    """ Master plan model for a project """
+
+    name = models.CharField(max_length=75, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Detail(models.Model):
+
+    """ Detail model for an entire project info """
+
+    master_plan = models.ForeignKey(to=MasterPlan, on_delete=models.CASCADE)
+    work_axe = models.ForeignKey(to=WorkAxe, default="", on_delete=models.SET_DEFAULT)
+    activity = models.ForeignKey(to=Activitie, default="", on_delete=models.SET_DEFAULT)
+    responsible = models.ForeignKey(to=Responsible, default="", on_delete=models.SET_DEFAULT)
+    scheduled_date = models.DateField(null=False, blank=False)
+    completed_date = models.DateField(null=False, blank=False)
+    evaluation = models.TextField(null=True, blank=True)
+    observations = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+
+        return f'{self.master_plan} - {self.activity}'
